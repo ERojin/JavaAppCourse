97592d0d9371d8a18ab6dc111e157b3375765511
Eylul Rojin <eylulrojingunduz@gmail.com>
2023-07-03 15:51:50 +0300

    Programın komut satırı argümanlarıyla verilen gün ay ve yıl bilgisiyle verilen doğum tarihi bilgisine ilişkin doğum gününün durumuna göre aşağıdaki işlemler yapan programa ait kütüphane  yazıldı.

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,21 +1,42 @@
 package org.csystem.app;
+
 import com.karandev.util.console.Console;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static com.karandev.util.console.commandline.CommandLineUtil.*;
+
 class Application {
-    public static void run(String [] args)
+    public static void run(String[] args)
     {
-        Sample.foo(3);
-    }
-}
-class Sample {
-    public static void foo(int n)
-    {
-        Console.writeLine("Giriş:%d", n);
+        checkLengthEquals(args.length,3, "Wrong number of arguments");
+       try {
+            int day = Integer.parseInt(args[0]);
+            int month = Integer.parseInt(args[1]);
+            int year = Integer.parseInt(args [2]);
 
-        if (n==0)
-            return;
+            day = Console.readInt("Doğduğunuz gününü giriniz:" );
+            month = Console.readInt("Doğduğunuz ayı giriniz:" );
+            year = Console.readInt("Doğduğunuz yılı giriniz:" );
 
-        foo(n-1);
-        Console.writeLine("Çıkış:%d", n);
+           var BirthDay = new GregorianCalendar(day, month, year);
+
+           var now = new GregorianCalendar();
+
+           if (now.after(BirthDay))
+               Console.writeLine("Geçmiş doğum gününüz kutlu olsun");
+           else if (BirthDay.after(now))
+               Console.writeLine("Doğum günününüz şimdiden kutlu olsun");
+           else
+               Console.writeLine("Doğum gününüz kutlu olsun");
+        }
+        catch (NumberFormatException ignore) {
+            Console.Error.writeLine("Invalid arguments");*/
+        }
+
+
+
+
     }
 }
