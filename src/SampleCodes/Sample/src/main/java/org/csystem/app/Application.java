5ecba2811eceb03d330237d427199a4295f3e056
Eylul Rojin <eylulrojingunduz@gmail.com>
2023-07-11 21:42:37 +0300

    DateTimeLib kütüphanesi güncellendi.

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,12 +1,14 @@
 package org.csystem.app;
 
 import com.karandev.util.console.Console;
+import org.csystem.util.datetime.converter.DateTimeConverterUtil;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -15,10 +17,13 @@ import static com.karandev.util.console.commandline.CommandLineUtil.*;
 class Application {
     public static void run(String[] args)
     {
-        var now = LocalDateTime.now();
+        var a = 0x80000B00; //0b1000_0000_0000_0000_0000_1011_0000_0000
+        var b = 0x00000B00; //0b0000_0000_0000_0000_0000_1011_0000_0000
 
-        Console.writeLine(DateTimeFormatter.ofPattern("dd / MM / yyyy kk:mm:ss E").format(now));
-        Console.writeLine(DateTimeFormatter.ofPattern("d / MMM / yyyy KK:mm:ss e B").format(now));
+        Console.writeLine("a = %d " , a);
+        Console.writeLine("b = %d ", b );
+
+
 
     }
 }
